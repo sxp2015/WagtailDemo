7f5d328a1e97c4fb54b2fcdc58e4f07f2b864542
‘15575602407’ <15575694746@qq.com>
2023-02-21 08:26:43 +0800

    init

---
+++ home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.admin.panels import RichTextFieldPanel
+
+class HomePage(Page):
+    pass
---
+++ wagtail_app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-uozna2+v_q-b=)j_!(i49o_^thwl^luch&26itvq(ah2x_3wwt"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
